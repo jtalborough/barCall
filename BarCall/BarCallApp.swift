7832d478ff46e294b20bef86f6140c57990837e2
Jason T Alborough <jtalborough@gmail.com>
2023-08-02 07:06:00 -0400

    Initial Commit

---
+++ BarCall/BarCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCallApp.swift
+//  BarCall
+//
+//  Created by Jason T Alborough on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BarCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarCall
+//
+//  Created by Jason T Alborough on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
