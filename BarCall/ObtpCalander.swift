0201da9a95b9114dda40666aa757bdfed2e8ffb2
Jason T Alborough <jtalborough@gmail.com>
2023-08-08 15:15:30 -0400

    feature: adds fuzzy relative times, on by dfefualt currently
    fix: relative time formatting

--- BarCall/ObtpCalander.swift
+++ BarCall/ObtpCalander.swift
@@ -26,7 +26,7 @@ class ObtpCalendar : ObservableObject {
             let eventStartTime = MyEvents[0].Event.startDate
             let timeDifference = Calendar.current.dateComponents([.minute], from: now, to: eventStartTime!).minute ?? 0
 
-            if abs(timeDifference) <= 10 {
+            if timeDifference <= 10 {
                 // Prepend a bell icon if the event starts within 10 minutes
                 title = "🔔 " + MyEvents[0].Title + " • " + MyEvents[0].RelativeStartTime
             } else {
@@ -48,19 +48,8 @@ class ObtpCalendar : ObservableObject {
         MyEvents.removeAll()
         for tempevent in sortedEvents {
             if tempevent.endDate.timeIntervalSinceNow > 0 && !tempevent.isAllDay {
-                
+
                 /*
-                // Ask for the full relative date
-                let formatter = RelativeDateTimeFormatter()
-                formatter.unitsStyle = .full
-                formatter.dateTimeStyle = .named
-
-                // Get tempevent.startDate relative to the current date
-                
-                let relativeDate = formatter.localizedString(for: tempevent.startDate, relativeTo: Date())
-
-                let newEvent = Events(title: tempevent.title, startTime: relativeDate, event: tempevent)
-                */
                 let currentDate = Date()
                 let eventDate = tempevent.startDate!
 
@@ -74,8 +63,9 @@ class ObtpCalendar : ObservableObject {
                 formatter.unitsStyle = .full
                 formatter.maximumUnitCount = 2 // Display at most 2 units (e.g., 1 hour, 45 minutes)
                 let relativeDate = formatter.string(from: components) ?? ""
-
-                let newEvent = Events(title: tempevent.title, startTime: "in \(relativeDate)", event: tempevent)
+                */
+                
+                let newEvent = Events(title: tempevent.title, startTime: formatRelativeTime(to: tempevent.startDate), event: tempevent)
                 
                 let location = tempevent.location
 
@@ -84,17 +74,57 @@ class ObtpCalendar : ObservableObject {
                 }
                 else {
                     newEvent.Url = URL(string: extractTeamsURL(from: tempevent.notes ?? "") ?? "")
-
-                    
                 }
 
                 MyEvents.append(newEvent)
-                print(tempevent.title!, "at", relativeDate)
+                //print(tempevent.title!, "at", relativeDate)
             }
         }
         NextEvent = getNextEventTitle()
     }
 
+    enum TimeFormat {
+        case exact
+        case fuzzy
+    }
+
+    func formatRelativeTime(to eventDate: Date, format: TimeFormat = .fuzzy) -> String {
+        let currentDate = Date() // Get the current date directly within the function
+        
+        // Calculate the difference between the two dates
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: currentDate, to: eventDate)
+
+        var hours = components.hour ?? 0
+        var minutes = components.minute ?? 0
+
+        if format == .fuzzy {
+            // Round minutes to the nearest 5
+            minutes = (minutes + 2) / 5 * 5
+            if minutes == 60 {
+                hours += 1
+                minutes = 0
+            }
+        }
+
+        // Format the difference using DateComponentsFormatter
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        formatter.unitsStyle = .full
+        formatter.maximumUnitCount = 2 // Display at most 2 units (e.g., 1 hour, 45 minutes)
+
+        if let formattedString = formatter.string(from: DateComponents(hour: hours, minute: minutes)) {
+            if eventDate < currentDate {
+                // If the eventDate is in the past
+                return format == .fuzzy ? "about \(formattedString.replacingOccurrences(of: "-", with: "")) ago" : "\(formattedString.replacingOccurrences(of: "-", with: "")) ago"
+            } else {
+                // If the eventDate is in the future
+                return format == .fuzzy ? "about \(formattedString)" : "in \(formattedString)"
+            }
+        }
+        return ""
+    }
+    
     func extractTeamsURL(from text: String) -> String? {
         let pattern = "<(https://teams\\.microsoft\\.com/l/meetup-join/[^\n]+)>"
         let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
